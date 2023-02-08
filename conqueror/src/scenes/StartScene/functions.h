f5e6f8279062f3ce33fd554dd191615ca48a80ba
levlau <levlau@web.de>
2023-02-08 12:12:19 +0100

    continued with creategrid function

--- conqueror/src/scenes/StartScene/functions.h
+++ conqueror/src/scenes/StartScene/functions.h
@@ -4,14 +4,28 @@
 
 using namespace core;
 
-GameObject* CreateGrid(const uint8_t x_size, const uint8_t y_size, const float space, const glm::vec2 mid_pos) {
-	std::vector<std::vector<GameObject>> *grid = new std::vector<std::vector<GameObject>>;	//create 2D vector on the heap
+
+// TODO: Change to vec2 2D-vector, cubes are placeholders for debugging and visualisation
+std::vector<GameObject> CreateGrid(const uint8_t x_size, const uint8_t y_size, const float space, const glm::vec2 mid_pos) {
+
+	std::vector<std::vector<GameObject>> grid;	//create 2D vector
+	float cube_rad = 0.25f;
+	glm::vec2 whole_len = glm::vec2((x_size * cube_rad * 2.0f) + (x_size - 1) * offset, y_size * cube_rad * 2.0f) + (y_size - 1) * offset);		//length of all cubes + offsets between
+	glm::vec2 start_pos = glm::vec2(mid_pos.x - whole_len.x / 2.0f, mid_pos.y + whole_len.y / 2.0f);	// starts top left
+
 	for (size_t x = 0; x < x_size; x++) {
-		std::vector<GameObject> *y_row = new std::vector<GameObject>;	// create new y row vector 
+		std::vector<GameObject> y_row;	// create new y row vector 
 		for (size_t y = 0; y < y_size; y++) {	
-			y_row->push_back(GameObject("", Transform(glm::vec2(), glm::vec2())));	// add gameobjects to it
-			y_row->at(y).addComponent(new SpriteRenderer(glm::vec4(0.0f, 0.0f, 1.0f, 0.5f)));	// add SpriteRenderer
+			y_row.push_back(new GameObject("", Transform(glm::vec2(), glm::vec2())));	// add gameobjects to it
+			y_row.at(y).addComponent(new SpriteRenderer(glm::vec4(0.0f, 0.0f, 1.0f, 0.5f)));	// add SpriteRenderer
 		}
-		grid->at(x).push_back(*y_row);	// add y_row to grid at index x
+		grid.at(x).push_back(y_row);	// add y_row to grid at index x
 	}
+	return grid;
 }
+
+
+
+//glm::vec2 CreateGrid(const uint8_t x_size, const uint8_t y_size, const float space, const glm::vec2 mid_pos) {
+//
+//}
