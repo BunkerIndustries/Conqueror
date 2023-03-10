7f8e988a7d4e442433491607c2ba9181e2a3fd89
levi <levlau@web.de>
2023-03-10 17:03:24 +0100

    bullets are still not working

--- conqueror/src/components/bullet.cpp
+++ conqueror/src/components/bullet.cpp
@@ -13,33 +13,31 @@ Bullet::Bullet(GameObject* target, bool is_hit, float damage)
 		target_position.y = target->transform.position.y;
 		target_position.x = target->transform.position.x + RandomF(min_inaccuracy, max_inaccuracy) * RandomInt(-1, 1);
 	}
-
-	LOG_DEBUG("Bullet ctor()");
 	LOG_DEBUG("target_position: x:{0}, y:{1}", target_position.x, target_position.y);
 }
 
 Bullet::~Bullet() {
-	LOG_DEBUG("Bullet ~Bullet()");
 }
 
 void Bullet::start() {
 	gameObject->AddComponent(new Movement(bullet_speed));
 	gameObject->GetComponent<Movement>()->target_position = target_position;
-	LOG_DEBUG("Bullet start()");
 }
 
 void Bullet::stop() {
-	LOG_DEBUG("Bullet stop()");
 }
 
 void Bullet::update(float dt) {
-	LOG_DEBUG("Bullet update()");
-	LOG_DEBUG("transform.position: x:{0}, y:{1}", gameObject->transform.position.x, gameObject->transform.position.y);
+	//LOG_DEBUG("transform.position: x:{0}, y:{1}", gameObject->transform.position.x, gameObject->transform.position.y);
 	if (gameObject->transform.position == target_position) {
+		LOG_DEBUG("Bullet at target position: x:{0}, y:{1}", gameObject->transform.position.x, gameObject->transform.position.y);
 		if (is_hit) {
-			LOG_DEBUG("on target position");
+			LOG_DEBUG("bullet hit");
 			target->GetComponent<Health>()->TakeDamage(damage);
 		}
+		else { LOG_DEBUG("bullet not hit"); }
+		
 		gameObject->Delete();
+		LOG_DEBUG("Bullet deleted");
 	}
 }
