f3fe07b267764b8fc474949450d414c0b91ec49d
matthias <matthias05.geng@t-online.de>
2023-03-12 22:48:08 +0100

    Test map

--- conqueror/src/required/map_creation.h
+++ conqueror/src/required/map_creation.h
@@ -23,27 +23,54 @@ inline void CreateGameMap(std::vector<std::pair<std::vector<glm::vec2>, Stand>>&
 inline std::vector<std::pair<std::vector<glm::vec2>, Stand>> standard_map = {
 	{
 		{
-			glm::vec2(-1.0f, -1.0f)
+			glm::vec2(-7.0f, -8.0f),
+			glm::vec2(-6.0f, -8.0f),
+			glm::vec2(-7.0f, -7.0f),
+			glm::vec2(-6.0f, -7.0f)
 		}
 		,waiting_stand
 	},
 	{
 		{
-			glm::vec2(-1.0f, -2.0f)
+			glm::vec2(8.0f, -4.0f),
+			glm::vec2(7.0f, -4.0f),
+			glm::vec2(6.0f, -3.75f),
+			glm::vec2(5.0f, -3.5f),
+			glm::vec2(2.0f, -3.0f),
+			glm::vec2(1.0f, -3.0f),
+			glm::vec2(0.0f, -3.0f),
+			glm::vec2(-1.0f, -3.0f),
+			glm::vec2(-2.0f, -3.0f),
+			glm::vec2(-5.0f, -3.5f),
+			glm::vec2(-6.0f, -3.75f),
+			glm::vec2(-7.0f, -4.0f),
+			glm::vec2( - 8.0f, -4.0f),
 		}
-		,artillerie_stand
+		,trench_stand
 	},
 	{
 		{
-			glm::vec2(-1.0f, -3.0f),
-			glm::vec2(-2.0f, -3.0f),
-			glm::vec2(-3.0f, -3.0f),
 			glm::vec2(-4.0f, -3.0f),
-			glm::vec2(-5.0f, -3.0f),
-			glm::vec2(-6.0f, -3.0f),
-			glm::vec2(-7.0f, -3.0f),
-			glm::vec2(-8.0f, -3.0f)
+			glm::vec2(-4.0f, -3.75f),
+			glm::vec2(-3.0f, -3.75f),
+			glm::vec2(-3.0f, -3.0f),
+
+
+			glm::vec2(4.0f, -3.0f),
+			glm::vec2(4.0f, -3.75f),
+			glm::vec2(3.0f, -3.75f),
+			glm::vec2(3.0f, -3.0f),
 		}
-		,trench_stand
+		,bunker_stand
+	},
+	{
+		{
+			glm::vec2(-2.0f, -4.75f),
+			glm::vec2(-6.0f, -5.5f),
+			glm::vec2(6.0f, -5.5f),
+			glm::vec2(2.0f, -4.75f),
+			glm::vec2(0.0f, -5.5f)
+		}
+		,hiding_stand
 	}
 };
