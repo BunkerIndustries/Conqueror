e1c1969e45639d45f46e22a78e4ea56209676e20
TheTrialGamer <pascal.gutsche@gutsche-gmbh.de>
2023-04-11 19:14:45 +0200

    fixed line rendering because of merge

--- core/src/core/component/LineRenderer.cpp
+++ core/src/core/component/LineRenderer.cpp
@@ -7,14 +7,14 @@
 
 namespace core
 {
-	LineRenderer::LineRenderer(glm::vec2 positionA, glm::vec2 positionB, glm::vec4 color, float thickness)
-		:positionA(positionA), positionB(positionB), color(color), thickness(thickness) { }
+	LineRenderer::LineRenderer(glm::vec2 positionA, glm::vec2 positionB, glm::vec4 color, float thickness, float lastingTime)
+		:positionA(positionA), positionB(positionB), color(color), thickness(thickness), lastingTime(lastingTime) { }
 
 	void LineRenderer::OnUpdate()
 	{
 		dt_counter += Application::GetDT();
 		Renderer::DrawLine(positionA, positionB, color, thickness, gameObject->GetProjectionMode(), gameObject->GetObjectID());
 
-		if (dt_counter >= lasting_time) delete gameObject;
+		if (dt_counter >= lastingTime) delete gameObject;
 	}
 };
